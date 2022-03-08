d7c008a334272ed2ddcacb1bf37991cb502ed398
Rebecca Stone <you@example.com>
2022-03-08 17:34:24 -0600

    made the todos appear on screen. can also mark them as completed

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,114 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import axios from 'axios';
 
 const URL = 'http://localhost:9000/api/todos'
 
+const initialState = {
+  successMessage: '',
+  errorMessage: '',
+  todo: [],
+  form: {
+    name: '',
+    completed: false,
+  }
+}
+
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = initialState
+  }
+
+  componentDidMount(){
+    this.getTodo()
+  }
+  
+  getTodo = () => {
+    axios.get(URL)
+    .then(res => {
+      console.log('this is the getTodo res', res.data)
+      this.setState({
+        ...this.state, 
+        todo: res.data.data,
+        successMessage: res.data.message
+      })
+    })
+    .catch(err => {
+      debugger
+      this.setState({ ...this.state, errorMessage: 'ERROR' })
+    })
+  }
+
+  addTodo = () => {
+    const newTodo = {
+      name: this.state.form.name
+    }
+    axios.post(URL, newTodo)
+    .then(res => {
+      console.log('this is addTodo res', res)
+      this.setState({
+        ...this.state,
+        todo: [ ...this.state.todo, res.data.data]
+      })
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        errorMessage: err.response.data.message,
+      })
+    })
+  }
+
+  completeTodo = id => {
+    axios.patch(`${URL}/${id}`)
+    .then(res => {
+      console.log('this is the completed', res.data.data)
+      this.setState({
+        ...this.state,
+        successMessage: res.data.data.message,
+        todo: this.state.todo.map(data => {
+          console.log('this is the data', res.data.data.patched_todo)
+          return data.id == id ? res.data.data : data
+        })
+      })
+    })
+    .catch(err => {
+      this.setState({
+        ...this.state,
+        errorMessage: err.response.data.message,
+      })
+    })
+  }
+
+  changeTodo = (key, value) => {
+    this.setState({
+      ...this.state,
+      form: { ...this.state.form, [key]: value }
+    })
+  }
+
   render() {
-    return null
+    const { todo, form } = this.state
+    const { foo } = this.props
+    return (
+      <div>
+        <h2>Todo List: {foo}</h2>
+        {this.state.successMessage}
+        {this.state.errorMessage}
+        <ul>
+          {todo.map((item) => {
+            const { id, name, completed } = item
+            return (
+              <li key={id}>
+                {completed == true ? `${name} is done!` : `${name} needs work`}
+                <button onClick={evt => this.completeTodo(id)}>finished</button>
+                </li>
+            )
+          })}
+        </ul>
+        <Form onChange={this.changeTodo} values={form} onSubmit={this.addTodo} />
+      </div>
+    )
   }
 }
